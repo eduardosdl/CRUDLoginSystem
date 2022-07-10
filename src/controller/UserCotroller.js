ebf3e310fdf538b6e94be107a4dea368c5fce338
eduardosdl <edu.silva.e08@gmail.com>
2022-07-10 09:18:19 -0300

    added cryptography

--- src/controller/UserCotroller.js
+++ src/controller/UserCotroller.js
@@ -1,5 +1,7 @@
+const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
 const User = require('../models/User');
+
 
 // rota post /user/login
 const validate = async (req, res) => {
@@ -26,31 +28,14 @@ const register = async (req, res) => {
     res.render('pages/new')
 }
 
-// rota post /user/registered
-/* const registered = async (req, res) => {
-    const dados = req.body;
-    
-    console.log(dados);
-
-    User.create({
-        name: req.body.nome,
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email
-    }).then(() => {
-        res.render("/");
-        console.log('criado com sucesso');
-    }).catch((err) => {
-        console.log('houve um erro: '+err);
-        res.render("pages/new", {msg: "erro ao adiconar no banco"})
-    }); 
-}*/
-
+// rota para cadastro do user -> post /user/registered
 const registered = async (req, res) => {
+    // validação de senha
     if(req.body.password != req.body.password2) {
         req.flash('error_msg', 'As senhas são diferentes');
         res.redirect("/user/register");
     }else {
+        // consulta ao banco para para ver se ja existe o usuario
         const consulta = await User.findAll({
             attributes: ['username'],
             where: {
@@ -62,25 +47,40 @@ const registered = async (req, res) => {
             console.log(err);
         });
 
-        console.log(consulta);
-
         if(consulta.length > 0) {
             req.flash('error_msg', 'Já exite uma conta com esse usuário');
             res.redirect('/user/register');
         } else {
-            User.create({
+            // objeto para criação do usuário
+            const newUser = {
                 name: req.body.nome,
                 username: req.body.username,
                 email: req.body.email,
                 password: req.body.password
-            }).then(() => {
-                req.flash('success_msg', 'Conta criada com sucesso, faça login');
-                res.redirect("/");
-            }).catch((err) => {
-                req.flash('error_msg', 'Houve um erro interno');
-                res.redirect('/user/register');
-                console.log('houve um erro: '+err);
-            }); 
+            }
+
+            // gerador de hash para a senha
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) {
+                        req.flash('error_msg', 'Houve um erro interno');
+                        res.redirect("/user/register");
+                    }
+
+                    // salva o hash no lugar da senha original
+                    newUser.password = hash;
+
+                    // cria o usuário no banco de dados
+                    User.create(newUser).then(() => {
+                        req.flash('success_msg', 'Conta criada com sucesso, faça login');
+                        res.redirect("/");
+                    }).catch((err) => {
+                        req.flash('error_msg', 'Houve um erro ao criar o usuario, tente novamente');
+                        res.redirect('/user/register');
+                        console.log('houve um erro: '+err);
+                    });
+                });
+            });
         }
 
     }
